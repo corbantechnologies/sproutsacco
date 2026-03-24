7b6b7d78975a68aa9fc3d5e946301a752772b983
Dalienst <oduordalienst@gmail.com>
2026-03-24 10:56:23 +0300

    fix select error

--- forms/glaccounts/UpdateGLAccount.jsx
+++ forms/glaccounts/UpdateGLAccount.jsx
@@ -8,6 +8,13 @@ import {
     DialogTitle,
     DialogFooter,
 } from "@/components/ui/dialog";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
